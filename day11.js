ab6ed0654b8649fdaa255dc18838f8e7028493b0
educationalgamer <94797176+educationalgamer@users.noreply.github.com>
2023-02-19 22:21:57 +0530

    Add files via upload

--- day11.js
+++ day11.js
@@ -1,3 +1,5 @@
+// call back function
+
 function greet(name,callback){
     console.log(`hello ${name}`)
     callback();
@@ -6,3 +8,24 @@ function goodbuy(){
     console.log(`goodbuy`)
 }
 greet('anshu',goodbuy)
+
+
+//arrow function
+var iseven= (elements)=>{
+    // if(elements%2===0){
+    //     return true;
+    // }
+    // return false;
+    return elements %2===0;
+};
+// console.log(iseven(4));
+
+// var result=[3,6,8,2].every(iseven);
+var result=[4,6,8,2].every((e)=>{
+    return e%2===0;
+});
+console.log(result);
+//or
+
+var result=[3,6,8,2].some((e)=>e%2===0);
+console.log(result);
