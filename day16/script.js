5fba618bb92e7870cf0d47b4fd7c489d9d9437b8
educationalgamer <94797176+educationalgamer@users.noreply.github.com>
2023-02-28 19:40:21 +0530

    file added

---
+++ day16/script.js
@@ -0,0 +1,14 @@
+var counter=document.querySelector('.counter');
+var follower=document.querySelector('.followers');
+
+let count=1;
+setInterval(()=>{
+if (count<1000) {
+    count++;
+    counter.innerHTML=count;
+}
+},1)
+
+setTimeout(() => {
+    follower.innerHTML="followers on instagram!";
+}, 5000);
